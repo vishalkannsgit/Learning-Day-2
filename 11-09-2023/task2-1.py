740a9dea85089ef6f3aebf6fb72152698e77b227
vishal kanns <136556305+vishalkannsgit@users.noreply.github.com>
2023-09-12 20:59:05 +0530

    Add files via upload

---
+++ 11-09-2023/task2-1.py
@@ -0,0 +1,6 @@
+# Read two integers from STDIN
+A = int(input("Enter the value of A : "))
+B = int(input("Enter the value of B : "))
+print(A + B)            # Calculate and print the sum of the two numbers
+print(A - B)            # Calculate and print the difference of the two numbers 
+print(A * B)            # Calculate and print the product of the two numbers
